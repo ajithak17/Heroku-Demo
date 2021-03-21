385e66e86c2d9df2e46153ef0a3b374eda7e9462
ajithak17 <79757399+ajithak17@users.noreply.github.com>
2021-03-21 22:45:20 +0530

    final

--- app.py
+++ app.py
@@ -1,57 +1,116 @@
-from flask import Flask,render_template,request
-import requests,json
+<html>
 
-from werkzeug.utils import secure_filename
-import os
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Classifier</title>
+    <link
+      rel="icon"
+      type="image/png"
+      href="https://www.google.com/s2/favicons?domain=gnanaganesh-resume.web.app"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
+    />
+    <link rel="stylesheet" type="text/css" href="{{url_for('static',filename='styles.css')}}" />
+</head>
 
-import tensorflow as tf
-import numpy as np
-import pathlib
 
-import requests
-from io import BytesIO
-
-app = Flask(__name__)
+          
+   
+<body>
+         
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+   <form id="imgur" method="POST" enctype="multipart/form-data" >
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    return render_template('upload.html')
+
+   <div class="quote-container" id="quote-container">
+      <div class="quote-text">
+        <span id="quote">
+
+          
+
+
+         {% if data %}
+         <h4 id="text">Your Crop name:</h4><p>{{ data }}</p>
+          {% endif %}
+
+        </span>
+      </div>
+  </div>
+
+     
+     
+</form>
+
+</body>
+    <script src="scripts.js"></script>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
+<script>
+    $("document").ready(function () {
+
+        $('input[type=file]').on("change", function () {
+
+            var $files = $(this).get(0).files;
+
+            if ($files.length) {
+
+                // Reject big files
+                if ($files[0].size > $(this).data("max-size") * 1024) {
+                    console.log("Please select a smaller file");
+                    return false;
+                }
+
+                // Replace ctrlq with your own API key
+                var apiUrl = 'https://api.imgur.com/3/image';
+                var apiKey = '5b103bbe3e1ff42';
+
+                var formData = new FormData();
+                formData.append("image", $files[0]);
+
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": apiUrl,
+                    "method": "POST",
+                    "datatype": "json",                
+                    "headers": {
+                        "Authorization": "Client-ID " + apiKey
+                    },
+                    "processData": false,
+                    "contentType": false,
+                    "data": formData,
+                    beforeSend: function (xhr) {
+                        console.log("Uploading | 上传中");
+                    },
+                    success: function (res) {
+                        $('form').append('<input type="hidden" name="hidden" value="'+res.data.link+'">');
+                                                 console.log(res.data.link);
+      },
+                    error: function () {
+                        alert("Failed | 上传失败");
+                    }
+                }
+                $.ajax(settings).done(function (response) {
+                    console.log("Done | 成功");
+                });
+            }
+        });
+    });
+</script>
+
+
+        
+    </div>
 
     
 
-
-@app.route('/classify', methods=['POST'])
-def classify():
-    from tensorflow.keras.models import load_model
-    model=load_model('model.h5')
-    from tensorflow.keras.preprocessing import image
-    if request.method == 'POST':
-        # Get the file from post request
-        f=request.files['file']
-        path = request.form.get('hidden')
-   
-        classes=['maize','rice','sugarcane','wheat']
-
-        def scale(image):
-            image = tf.cast(image, tf.float32)
-            image /= 255.0
-            return tf.image.resize(image,[200,200])
-        def decode_img(image):
-            img = tf.image.decode_jpeg(image, channels=3)
-            img = scale(img)
-            return np.expand_dims(img, axis=0)
-        
-        content = requests.get(path).content
-        result =np.argmax(model.predict(decode_img(content)),axis=1)
-        return render_template('result.html', data=classes[result[0]],p=path)
-
-     
-  
-        
-if __name__ == '__main__':
-    app.run()
+</div>
+</html>
